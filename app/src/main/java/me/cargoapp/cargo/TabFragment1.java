a00595ea2c11e7a7cded889a0b79c6390590c63f
GautheyValentin <beian77@hotmail.fr>
2017-04-14 18:14:01 +0200

    :sparkles: Implement PLACE API but bug onResume

--- app/src/main/java/me/cargoapp/cargo/TabFragment1.java
+++ app/src/main/java/me/cargoapp/cargo/TabFragment1.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -20,10 +21,13 @@ import android.widget.Toast;
 
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
@@ -31,15 +35,23 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.location.places.ui.SupportPlaceAutocompleteFragment;
+import com.orhanobut.logger.Logger;
 
 import java.util.Locale;
 
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
 import static android.content.ContentValues.TAG;
 import static android.provider.UserDictionary.Words.APP_ID;
 
 import static android.R.attr.button;
 
-public class TabFragment1 extends Fragment implements ConnectionCallbacks, OnConnectionFailedListener {
+public class TabFragment1 extends android.support.v4.app.Fragment implements ConnectionCallbacks, OnConnectionFailedListener {
     Button button;
     EditText start;
     EditText end;
@@ -51,6 +63,8 @@ public class TabFragment1 extends Fragment implements ConnectionCallbacks, OnCon
     double longitude;
 
     private final int MY_PERMISSION = 1;
+    int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -63,14 +77,16 @@ public class TabFragment1 extends Fragment implements ConnectionCallbacks, OnCon
                     .addApi(LocationServices.API)
                     .build();
         }
+
+
         View view = inflater.inflate(R.layout.tab_fragment_1, container, false);
+
         end = (EditText)view.findViewById(R.id.end);
         button = (Button) view.findViewById(R.id.btn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i("TAG", "DEBUT");
-                mend = end.getText().toString();
                 Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
                         Uri.parse("google.navigation:q=" + mend));
 
@@ -82,6 +98,38 @@ public class TabFragment1 extends Fragment implements ConnectionCallbacks, OnCon
 
         return view;
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+
+
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment) getActivity().getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+
+                mend = place.getAddress().toString();
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+
+
+        try {
+            Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN).build(getActivity());
+            startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+        } catch (GooglePlayServicesRepairableException e) {
+            // TODO: Handle the error.
+        } catch (GooglePlayServicesNotAvailableException e) {
+            // TODO: Handle the error.
+        }
+    }
+
 
     @Override
     public void onStart() {
