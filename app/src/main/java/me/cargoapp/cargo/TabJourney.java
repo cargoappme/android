55c63e127e748d5cb58750e63be1995958837617
Marvin Roger <nivramdu94@gmail.com>
2017-04-18 15:22:56 +0200

    :sparkles: Implement places API

--- app/src/main/java/me/cargoapp/cargo/TabJourney.java
+++ app/src/main/java/me/cargoapp/cargo/TabJourney.java
@@ -1,14 +1,65 @@
 package me.cargoapp.cargo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.OnActivityResult;
+import org.androidannotations.annotations.ViewById;
+
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
 
 @EFragment(R.layout.tab_journey)
 public class TabJourney extends Fragment {
+    final int AUTOCOMPLETE_REQUEST_CODE = 1;
 
+    @ViewById(R.id.go)
+    Button _btn;
+
+    @AfterViews
+    void afterViews() {
+
+    }
+
+    @Click(R.id.go)
+    void onGo() {
+        try {
+            Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_OVERLAY).build(getActivity());
+            startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);
+        } catch (GooglePlayServicesRepairableException e) {
+            // TODO: Handle the error.
+        } catch (GooglePlayServicesNotAvailableException e) {
+            // TODO: Handle the error.
+        }
+    }
+
+    @OnActivityResult(AUTOCOMPLETE_REQUEST_CODE)
+    void onResult(int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            Place place = PlaceAutocomplete.getPlace(getActivity(), data);
+            Toast.makeText(getActivity(), place.getAddress(), Toast.LENGTH_SHORT).show();
+        } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+            Status status = PlaceAutocomplete.getStatus(getActivity(), data);
+            // TODO: Handle the error.
+            Toast.makeText(getActivity(), status.getStatusMessage(), Toast.LENGTH_SHORT).show();
+
+        } else if (resultCode == RESULT_CANCELED) {
+            // The user canceled the operation.
+        }
+    }
 }
