e08859c3fcb9a684e88a9ed437670bf8f19bb15e
Mathieu Boissady <boissady.mathieu@gmail.com>
2017-05-05 10:06:26 +0200

    :art: add of the header parking and the parking button register you position

--- app/src/main/java/me/cargoapp/cargo/MainActivity.java
+++ app/src/main/java/me/cargoapp/cargo/MainActivity.java
@@ -1,28 +1,44 @@
 package me.cargoapp.cargo;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.OptionsItem;
 import org.androidannotations.annotations.OptionsMenu;
 import org.androidannotations.annotations.ViewById;
+
+import java.util.Locale;
 
 @EActivity(R.layout.activity_main)
 @OptionsMenu(R.menu.main)
 public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener {
 
     private String TAG = this.getClass().getSimpleName();
+    Double longi;
+    Double lat;
 
     @ViewById(R.id.pager)
     ViewPager _viewPager;
+
+    @ViewById(R.id.include)
+    View _header;
+
+    @ViewById(R.id.findPark)
+    Button _findPark;
 
     @AfterViews
     void afterViews() {
@@ -30,11 +46,29 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
         tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_journey));
         tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_vehicle));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if (preferences.contains("longitude") && preferences.contains("latitude")) {
+            _header.setVisibility(View.VISIBLE);
+        }
+        else {
+            _header.setVisibility(View.GONE);
+        }
         final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
         _viewPager.setAdapter(adapter);
         tabLayout.addOnTabSelectedListener(this);
     }
+    @Override
+    public void onResume(){
+        super.onResume();
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if (preferences.contains("longitude") && preferences.contains("latitude")) {
+            _header.setVisibility(View.VISIBLE);
+        }
+        else {
+            _header.setVisibility(View.GONE);
+        }
+    }
+
 
     @OptionsItem(R.id.action_settings)
     void onActionSettings() {
@@ -76,5 +110,19 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
         public int getCount() {
             return mNumOfTabs;
         }
+    }
+    @Click(R.id.findPark)
+    void onFind() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        longi = Double.longBitsToDouble(preferences.getLong("longitude", 0));
+        lat = Double.longBitsToDouble(preferences.getLong("latitude", 0));
+        String uri = String.format(Locale.ENGLISH, "http://maps.google.com/maps?daddr=%f,%f (%s)", lat, longi, "Parking");
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+        startActivity(intent);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.remove("longitude");
+        editor.remove("latitude");
+        editor.commit();
+        _header.setVisibility(View.GONE);
     }
 }
